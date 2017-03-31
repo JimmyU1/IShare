8bd1624e1a600482916462512d4811347f6e6549
JimmyU1 <ljn19940214@hotmail.com>
2017-03-31 11:29:25 +0800

    2017-03-31：delete import

--- src/main/java/com/ljn/controller/BaseController.java
+++ src/main/java/com/ljn/controller/BaseController.java
@@ -1,12 +1,9 @@
 package com.ljn.controller;
 
-import com.ljn.model.Contact;
 import com.ljn.service.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.security.acl.Owner;
 
 /**
  * Created by ljn19 on 2017-03-31.
